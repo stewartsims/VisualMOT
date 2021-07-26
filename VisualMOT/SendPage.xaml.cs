0b484d19ab5bb129a88b038bf55a0f3404f7175d
Stewart Sims <stewart@ssims.co.uk>
2021-07-26 16:44:18 +0100

    Email now working (including images) with MailKit

--- VisualMOT/SendPage.xaml.cs
+++ VisualMOT/SendPage.xaml.cs
@@ -1,10 +1,14 @@
-﻿using Org.Reddragonit.Stringtemplate;
+﻿using MailKit.Net.Smtp;
+using MailKit.Security;
+using MimeKit;
+using Org.Reddragonit.Stringtemplate;
 using Plugin.Toast;
 using Syncfusion.SfBusyIndicator.XForms;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
@@ -54,34 +58,32 @@ namespace VisualMOT
                 return new Command(async (parameter) =>
                 {
                     SfBusyIndicator loadingSpinner = (SfBusyIndicator)parameter;
-                    loadingSpinner.IsBusy = true;
+                    busyIndicator.IsBusy = true;
 
-                    List<EmailAttachment> attachments = new List<EmailAttachment>();
+                    MimeMessage message = new MimeMessage();
+
+                    // now create the multipart/mixed container to hold the message text and the
+                    // image attachment
+                    var multipart = new Multipart("mixed");
+
                     foreach (MOTItem item in MOTHistory.Items.Where(item => item.image != null))
                     {
                         string fileName = Path.GetTempFileName();
                         string file = Path.Combine(FileSystem.CacheDirectory, fileName);
                         File.WriteAllBytes(file, item.image);
-                        EmailAttachment attachment = new EmailAttachment(file);
-                        item.imageFileName = file;
+
+                        string contentId = Guid.NewGuid().ToString();
+                        multipart.Add(new MimePart("image", "jpg") {
+                            ContentObject = new ContentObject(File.OpenRead(file), ContentEncoding.Default),
+                            ContentDisposition = new ContentDisposition(ContentDisposition.Inline),
+                            ContentTransferEncoding = ContentEncoding.Base64,
+                            ContentId = contentId,
+                            FileName = fileName,
+                        }); ;
+                        item.imageFileName = contentId;
                     }
-                    /*
-                    msg.IsBodyHtml = true;
-                    Attachment inlineLogo = new Attachment(@"C:\Desktop\Image.jpg");
-                    msg.Attachments.Add(inlineLogo);
-                    string contentID = "Image";
-                    inlineLogo.ContentId = contentID;
-
-                    //To make the image display as inline and not as attachment
-
-                    inlineLogo.ContentDisposition.Inline = true;
-                    inlineLogo.ContentDisposition.DispositionType = DispositionTypeNames.Inline;
 
                     //To embed image in email
-
-                    msg.Body = "<htm><body> <img src=\"cid:" + contentID + "\"> </body></html>";
-                    */
-
                     string template;
                     if (MOTHistory.LastTest.testResult == "PASSED")
                     {
@@ -102,37 +104,56 @@ namespace VisualMOT
                     templator.SetAttribute("expiry", MOTHistory.LastTestExpiryDate);
 
                     List<string> items = new List<string>();
-                    foreach(MOTItem item in MOTHistory.Items)
+                    foreach (MOTItem item in MOTHistory.Items)
                     {
                         Template itemTemplator = new Template(itemTemplate);
                         itemTemplator.SetAttribute("severity", item.type);
                         itemTemplator.SetAttribute("description", item.text);
                         itemTemplator.SetAttribute("image", item.imageFileName);
-                        items.Add(itemTemplate.ToString());
+                        items.Add(itemTemplator.ToString());
                     }
 
                     templator.SetAttribute("items", string.Join("\n", items));
                     string body = templator.ToString();
 
+
+                    int port = System.Int32.Parse(Constants.MailPort);
+                    bool ssl = bool.Parse(Constants.MailSSL);
+
+                    ServicePointManager.ServerCertificateValidationCallback = (sender, certificate, chain, sslPolicyErrors) => true;
+
+                    SmtpClient client = new SmtpClient();
+                    client.Connect(Constants.MailHost, port, SecureSocketOptions.StartTls);
+                    client.Authenticate(Constants.MailUsername, Constants.MailPassword);
+                                      
+                    multipart.Add(new TextPart("html") { Text = body });
+                    message.Body = multipart;
+
+                    MailboxAddress fromAddress = new MailboxAddress(Constants.MailFrom, Constants.MailFromAddress);
+                    message.Subject = Constants.MailSubject;
+                    message.From.Add(fromAddress);
+
+                    if (!string.IsNullOrEmpty(CustomerEmail))
+                    {
+                        message.To.Add(new MailboxAddress(CustomerEmail));
+                    }
+                    if (!string.IsNullOrEmpty(YourEmail))
+                    {
+                        message.To.Add(new MailboxAddress(YourEmail));
+                    }
+
                     try
                     {
-                        var message = new EmailMessage
-                        {
-                            Subject = Constants.EmailSubject,
-                            Body = body,
-                            To = new List<string>() { CustomerEmail, YourEmail },
-                            Attachments = attachments
-                        };
-                        await Email.ComposeAsync(message);
-                        Navigation.PopModalAsync();
+                        client.Send(message);
                         CrossToastPopUp.Current.ShowToastSuccess("The MOT report has been successfully sent. Thank you for using Visual MOT");
-                    } 
-                    catch (Exception e)
-                    {
-                        await DisplayAlert("Error", "Failed to send, please try again", "OK");
+                        loadingSpinner.IsBusy = false;
+                        Navigation.PopAsync();
                     }
-                    
-
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.ToString());
+                        DisplayAlert("Error", "There was a problem sending the report, please try again.", "OK");
+                    }
                     loadingSpinner.IsBusy = false;
                 });
             }
