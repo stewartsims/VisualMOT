ad94823a81c2e2e3b119707be457609e51902fb6
Stewart Sims <stewart@ssims.co.uk>
2021-08-19 16:04:27 +0100

    Sorting app icon for Android and building new version

--- VisualMOT.Android/MainActivity.cs
+++ VisualMOT.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace VisualMOT.Droid
 {
-    [Activity(Label = "VisualMOT", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "VisualMOT", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
